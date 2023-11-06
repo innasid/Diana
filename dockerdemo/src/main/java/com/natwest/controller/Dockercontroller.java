6dc8c0e40f4fbc8330ba2fc69b0bc93f8c8b06f1
Diana <innasid@rbos.co.uk>
2023-11-06 12:05:55 +0530

    dock

---
+++ dockerdemo/src/main/java/com/natwest/controller/Dockercontroller.java
@@ -0,0 +1,15 @@
+package com.natwest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class Dockercontroller {
+
+    @GetMapping("/")
+    public String Welcome() {
+        return "Welcome to Docker Microservice";
+    }
+}
